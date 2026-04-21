527a6c386e98d974e153f31c62e8a122bfd616bf
Abd El-Rahman Mustafa <aamus2024@gmail.com>
2026-04-21 15:05:44 +0400

    update

--- API/Domain/Entities/AgendaItem/AgendaItemType.cs
+++ API/Domain/Entities/AgendaItem/AgendaItemType.cs
@@ -2,9 +2,6 @@ namespace API.Domain.Entities;
 
 public class AgendaItemType : BaseEntity
 {
-    public string Code { get; set; } = null!; // e.g. "DISCUSSION", "DECISION", "INFORMATION"
     public string Name { get; set; } = null!; // e.g. "Discussion", "Decision", "Information"
-    public string NameAr { get; set; } = null!; // e.g. "مناقشة", "قرار", "معلومات"
     public string Description { get; set; } = null!; // e.g. "Agenda items that require discussion among attendees."
-    public string DescriptionAr { get; set; } = null!; // e.g. "بنود جدول الأعمال التي تتطلب مناقشة بين الحضور."
 }
