f13adf9c7c2394fdb7bfb1a4b771824e9255c2be
Abd El-Rahman Mustafa <aamus2024@gmail.com>
2026-04-21 12:50:02 +0400

    cleaning

--- Client/src/app/pages/meeting/meeting-settings/manage/meeting-settings-manage.ts
+++ Client/src/app/pages/meeting/meeting-settings/manage/meeting-settings-manage.ts
@@ -1,35 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BaseComponent } from '../../../../core/components/base-component/base-component';
-import { MeetingSettingsService } from '../meeting-settings.service';
-import { UpsertMeetingSettingsDto } from '../meeting-settings.interface';
-import { ToastrService } from 'ngx-toastr';
-import { FormInput } from '../../../../shared/components/input/input';
 import { MeetingCategories } from '../meeting-categories/meeting-categories';
 import { MeetingTypes } from '../meeting-types/meeting-types';
 import { MeetingLevels } from '../meeting-levels/meeting-levels';
 import { MeetingQuorum } from '../meeting-quorum/meeting-quorum';
 @Component({
   selector: 'app-meeting-settings-manage',
-  imports: [CommonModule, ReactiveFormsModule, FormInput, MeetingCategories, MeetingTypes, MeetingLevels, MeetingQuorum],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MeetingCategories,
+    MeetingTypes,
+    MeetingLevels,
+    MeetingQuorum,
+  ],
   templateUrl: './meeting-settings-manage.html',
   styleUrl: './meeting-settings-manage.css',
 })
 export class MeetingSettingsManage extends BaseComponent implements OnInit {
+  activeTab = 'quorum';
 
-
+  tabs = [
+    { key: 'quorum', labelEn: 'Quorum', labelAr: 'النصاب', icon: 'fa-solid fa-users-line' },
+    { key: 'categories', labelEn: 'Categories', labelAr: 'التصنيفات', icon: 'fa-solid fa-tags' },
+    { key: 'types', labelEn: 'Types', labelAr: 'الأنواع', icon: 'fa-solid fa-layer-group' },
+    { key: 'levels', labelEn: 'Levels', labelAr: 'المستويات', icon: 'fa-solid fa-chart-bar' },
+  ];
+  
   ngOnInit(): void {
     this.setPageTitle(this.lang() === 'en' ? 'Meeting Settings' : 'إعدادات الاجتماع');
   }
-
-
-  activeTab = 'quorum';
-
-tabs = [
-  { key: 'quorum',     labelEn: 'Quorum',     labelAr: 'النصاب',     icon: 'fa-solid fa-users-line' },
-  { key: 'categories', labelEn: 'Categories', labelAr: 'التصنيفات',  icon: 'fa-solid fa-tags' },
-  { key: 'types',      labelEn: 'Types',      labelAr: 'الأنواع',    icon: 'fa-solid fa-layer-group' },
-  { key: 'levels',     labelEn: 'Levels',     labelAr: 'المستويات',  icon: 'fa-solid fa-chart-bar' },
-];
 }
